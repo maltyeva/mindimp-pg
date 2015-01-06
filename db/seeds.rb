a891558b0706c628c6952a2081219942074a0d24
Maria <maria@Marias-MacBook-Pro-3.local>
2015-01-06 14:31:46 +0800

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+  User.create! do |u|
+    u.first_name = Maria
+    u.last_name = Altyeva
+    u.email = maria@mindimp.com
+    u.is_admin = true
+  end
