25da3a4844b18a37dd71cde5eb3e4d5d35e8a3ef
Maria <maria@Marias-MacBook-Pro-3.local>
2015-01-07 21:46:50 +0800

    testing aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Paperclip Image handling
- config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_protocol => 'http',
-    :bucket => ENV['AWS_BUCKET'],
-    :s3_credentials => {
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+ # config.paperclip_defaults = {
+ #    :storage => :s3,
+ #    :s3_protocol => 'http',
+ #    :bucket => ENV['AWS_BUCKET'],
+ #    :s3_credentials => {
+ #      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+ #      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+ #    }
+ #  }
 end
