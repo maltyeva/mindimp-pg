0903b51bb6ce27fc6fb0cff82274f2767b13758d
Maria <maria@Marias-MacBook-Pro-3.local>
2015-01-07 20:10:06 +0800

    working on getting aws ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Paperclip Image handling
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['mindimp'],
-    :access_key_id => ENV['AKIAIVE5XD2EUTMKO7GA'],
-    :secret_access_key => ENV['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']
+ config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_protocol => 'http',
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-   }
+  }
 end
