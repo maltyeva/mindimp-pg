d1049d87e47db41528085e51bb3552f4de3d5f24
Maria <maria@Marias-MacBook-Pro-3.local>
2015-01-07 11:46:33 +0800

    added authentication to admin/student portal?

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
-class PagesController < ApplicationController
-	 include HighVoltage::StaticPage
+class PagesController < HighVoltage::PagesController
 
 
-	 before_filter :require_admin, :only => :admin_portal
+	 before_filter :require_admin
+
+	 # , :only => :admin_portal
 
 
-	 def admin_portal
-	 end
+	 # def admin_portal
+	 # end
 
-	 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
 
-
   resources :users
   resources :user_sessions
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
+
 
 
   get '/about'    => 'high_voltage/pages#show', id: 'about'
