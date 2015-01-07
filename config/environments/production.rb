1950816670aa845dd2d825011d4d9e51853326dd
Maria <maria@Marias-MacBook-Pro-3.local>
2015-01-07 20:12:10 +0800

    added production aws code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
  config.paperclip_defaults = {
     :storage => :s3,
     :s3_protocol => 'http',
+    :bucket => ENV['AWS_BUCKET'],
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
