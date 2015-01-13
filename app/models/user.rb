e61e6970797c7f8e686302fca112d1fde47b5b47
Maria <maria@Marias-MacBook-Pro-3.local>
2015-01-13 22:45:04 +0800

    changes to user_form

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: { case_sensitive: false }, 
-                    presence: true, 
+                    presence: true, :on => :create,
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
 
