8799fe0f023dce57f94be63e6a8836bf50f42391
Maria <maria@Marias-MacBook-Pro-3.local>
2015-01-13 10:20:18 +0800

    more formatting and layout changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,26 @@
 class User < ActiveRecord::Base
+
+
+ #attr_accessor :first_name, :last_name, :email
+
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, :on => :create
-  validates :password, confirmation: true, :on => :create
-  validates :password_confirmation, presence: true, :on => :create
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
 
-  validates :email, uniqueness: true, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: { case_sensitive: false }, 
+                    presence: true, 
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+
+
+
+  validates :password, length: { minimum: 6 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
 
 #creates profile pic. 
   has_attached_file :profile_photo, 
