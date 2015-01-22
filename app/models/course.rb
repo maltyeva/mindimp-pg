1d4e4761ba398f3005e749303dc852c8e8429780
Maria <maria.altyeva@gmail.com>
2015-01-22 15:10:56 +0800

    added attachments

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,16 @@ class Course < ActiveRecord::Base
 	has_many :course_sessions
 	validates_presence_of :title, :abbreviation, :description, :session_no, :course_category
 
+	#creating the syllabus
+
+	has_attached_file :syllabus 
+                    # :storage => :dropbox,
+                    # :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                    # :dropbox_options => {...}
+
+    validates_attachment_content_type :syllabus, :content_type => ['application/pdf', 'application/msword', 'text/plain']
+    validates_with AttachmentSizeValidator, :attributes => :syllabus, :less_than => 10.megabytes
+
+
+
 end
