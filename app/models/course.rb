c9e53e1c802ce2ddf6a61ee76ff9bfa214efbb5a
Maria <maria.altyeva@gmail.com>
2015-01-22 12:07:32 +0800

    validation for course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,6 @@ class Course < ActiveRecord::Base
 
 	belongs_to :course_category
 	has_many :course_sessions
-	validates_presence_of :title, :abbreviation, :description, :session_no
+	validates_presence_of :title, :abbreviation, :description, :session_no, :course_category
 
 end
