0b99018450be20d01d4c8a7268bde8b2de8deb9b
Maria <maria.altyeva@gmail.com>
2015-01-27 09:59:56 +0800

    worked on syllabus handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
  	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def enrolled? 
+    @course_session.include?(@current_user)
+  end 
+
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
