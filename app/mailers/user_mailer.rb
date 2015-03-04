464c90b3a04595951d2e7fcabd4f74d60914afbb
Maria <maria.altyeva@gmail.com>
2015-03-04 11:08:40 +0800

    fixed activation bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
   def activation_needed_email(user)
     @user = user
         if Rails.env.production?
-          @url  = "http://www.dry-citadel-6111.herokuapp.com/users/#{user.activation_token}/activate"
+          @url  = "https://dry-citadel-6111.herokuapp.com/users/#{user.activation_token}/activate"
       else
           # Our dev & test URL
           @url  = "http://localhost:3000/users/#{user.activation_token}/activate"
