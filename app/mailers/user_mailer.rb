56f87e126449e5c6d9feee493e3dc9d5ab0aa7bf
Maria <maria.altyeva@gmail.com>
2015-03-04 16:32:49 +0800

    added back to course areas

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -36,7 +36,7 @@ class UserMailer < ActionMailer::Base
     @user = user
     @url  = edit_password_reset_url(user.reset_password_token)
     mail(:to => user.email,
-         :subject => "Your password has been reset")
+         :subject => "Your MindImp password has been reset")
   end
 
 
